b8f1bb8969a02054f58ed43f3d512cc5e3031341
jani <jfreire84@hotmail.com>
2020-03-10 12:10:59 +0100

    Inicializando proyecto

---
+++ src/main/java/com/backend/cloudclinicas/BackendCloudclinicasApplication.java
@@ -0,0 +1,13 @@
+package com.backend.cloudclinicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCloudclinicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCloudclinicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/cloudclinicas/BackendCloudclinicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.cloudclinicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCloudclinicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
